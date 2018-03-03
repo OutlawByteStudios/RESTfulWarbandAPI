dbd10f1de3bdf62b5e7e5d3fa5ccefd7045f7c80
LuisEngelniederhammer <lifribjor@live.de>
2018-03-03 15:46:53 +0100

    First iteration of Handshake implementation - untested

--- PhoenixLauncher/src/Classes/API.php
+++ PhoenixLauncher/src/Classes/API.php
@@ -4,39 +4,53 @@ namespace PhoenixLauncher\src\Classes;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use PhoenixLauncher\src\Controller\HandshakeController;
 
 require_once __DIR__ . '/APIResponse.php';
-
 class API
 {
 	const VERSION = 0.1;
 	public function getVersion(ServerRequestInterface $request, ResponseInterface $response, array $args)
 	{
-		return APIResponse::create(
-				[
-						'message' => 'API version ' . self::VERSION ,
-						'status' => 'success'
-				], 200)->send($response);
-		
+		return APIResponse::create ( [ 
+				'message' => 'API version ' . self::VERSION,
+				'status' => 'success' 
+		], 200 )->send ( $response );
 	}
 	public function ping(ServerRequestInterface $request, ResponseInterface $response, array $args)
 	{
-		return APIResponse::create(
-				[
+		return APIResponse::create ( [ 
 				'message' => 'Pong. API is reachable',
-				'status' => 'success'
-				], 
-				200)->send($response);
+				'status' => 'success' 
+		], 200 )->send ( $response );
 	}
 	public function handshake(ServerRequestInterface $request, ResponseInterface $response, array $args)
 	{
-		$ip = $_SERVER['REMOTE_ADDR'];
-		$userAgenet = $_SERVER['HTTP_USER_AGENT'];
+		$ip = $_SERVER ['REMOTE_ADDR'];
+		$userAgent = $_SERVER ['HTTP_USER_AGENT'];
+		$uid = isset ( $args ['uid'] ) ? $args ['uid'] : '';
 		
+		$Handshake = new HandshakeController ( $ip, $userAgent, $uid );
+		
+		$processed = $Handshake->process ();
+		
+		if (! $processed)
+		{
+			return APIResponse::create ( [ 
+					'message' => 'Welcome back ' . $uid,
+					'status' => 'success' 
+			], 200 )->send ( $response );
+		}
+		else
+		{
+			return APIResponse::create ( [ 
+					'message' => 'Hello ' . $uid . ' you must be new, welcome!',
+					'status' => 'success' 
+			], 200 )->send ( $response );
+		}
 	}
 	public function obscureClient(ServerRequestInterface $request, ResponseInterface $response, array $args)
 	{
-		
 	}
 }
 
--- PhoenixLauncher/src/Controller/HandshakeController.php
+++ PhoenixLauncher/src/Controller/HandshakeController.php
@@ -1,20 +1,29 @@
 <?php
+
 namespace PhoenixLauncher\src\Controller;
+
+use PhoenixLauncher\src\Service\HandshakeService;
 
 class HandshakeController
 {
-	private $ip, $userAgent;
-	
-	
-	public function __construct(string $ip, string $userAgent)
+	private $ip, $userAgent, $uid;
+	public function __construct(string $ip, string $userAgent, string $uid)
 	{
 		$this->ip = $ip;
 		$this->userAgent = $userAgent;
-
+		$this->uid = $uid;
 	}
-	
 	public function process(): string
 	{
+		$HandshakeService = new HandshakeService ();
 		
+		if (! $HandshakeService->exists ( $this->uid ))
+		{
+			return $HandshakeService->write ( $this->ip, $this->useragent );
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
